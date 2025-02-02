36dba6e015d7d4df61d88eb9b774007db0ef23ca
Yuri1031 <142709785+Yuri1031@users.noreply.github.com>
2025-02-02 19:16:20 +0900

    商品購入機能_指摘反映②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  before_action :redirect_if_sold, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -53,6 +54,12 @@ class ItemsController < ApplicationController
 
   def ensure_correct_user
     return if @item.user_id == current_user.id
+
+    redirect_to root_path
+  end
+
+  def redirect_if_sold
+    return unless @item.sold?
 
     redirect_to root_path
   end
